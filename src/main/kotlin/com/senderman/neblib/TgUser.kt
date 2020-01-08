34e9035eefd0e5e3e6172239c4ebb937928f601b
Fyodor Doletov <doletov.fyodor@yandex.ru>
2020-01-08 18:18:11 +0300

    refactor TgUser class

--- src/main/kotlin/com/senderman/neblib/TgUser.kt
+++ src/main/kotlin/com/senderman/neblib/TgUser.kt
@@ -2,28 +2,14 @@ package com.senderman.neblib
 
 import org.telegram.telegrambots.meta.api.objects.User
 
-open class TgUser {
+open class TgUser(val id: Int, name: String) {
+    val name = if (name.isBlank()) "Без имени" else name
+        .replace("<", "&lt;")
+        .replace(">", "&gt;")
+        .replace("&", "&amp;")
 
-    val id: Int
-    val name: String
     val link: String
         get() = "<a href=\"tg://user?id=$id\">$name</a>"
 
-    constructor(id: Int, name: String) {
-        this.id = id
-        this.name = getSafeName(name)
-    }
-
-    constructor(user: User) {
-        this.id = user.id
-        this.name = getSafeName(user.firstName)
-    }
-
-    private fun getSafeName(name: String): String {
-        if (name.trim().isBlank()) return "Без имени"
-        return name
-            .replace("<", "&lt;")
-            .replace(">", "&gt;")
-            .replace("&", "&amp;")
-    }
+    constructor(user: User) : this(user.id, user.firstName)
 }
